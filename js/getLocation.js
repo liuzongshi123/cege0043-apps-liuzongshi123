d0700fced8016a391eefad79e32925f3f6b663cb
liuzongshi123 <uceszl9@ucl.ac.uk>
2020-03-31 16:29:22 -0400

    deleted useless files

--- js/getLocation.js
+++
@@ -1,9 +0,0 @@
-function getLocation() { 
-	alert('getting location'); 
-	navigator.geolocation.getCurrentPosition(getPosition); 
-} 
-
-function getPosition(position) { 
-	document.getElementById('showLocation').innerHTML = "Latitude: " + position.coords.latitude + 
-	"<br>Longitude: " + position.coords.longitude; 
-}
