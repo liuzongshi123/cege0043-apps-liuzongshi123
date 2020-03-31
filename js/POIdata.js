bc1f9bef96921230cfffdfc90de235cc4028ce61
liuzongshi123 <uceszl9@ucl.ac.uk>
2020-03-31 09:56:19 -0400

    FIX THE URL BUG

--- js/POIdata.js
+++ js/POIdata.js
@@ -1,7 +1,7 @@
 var POILayer;
 
 function getPOIData(){
-    var layerURL = "https://developer.cege.ucl.ac.uk:30251/getPOI";
+    var layerURL = "https://developer.cege.ucl.ac.uk:30283/getPOI";
     var testMarkerRed = L.AwesomeMarkers.icon({
     		icon: 'play',
     		markerColor: 'red'
