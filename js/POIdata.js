ca2a5ff1c66d3213ebc0b98a6dee4da8fba3d4e4
liuzongshi123 <uceszl9@ucl.ac.uk>
2020-03-31 10:11:58 -0400

    Try to fix the bug

--- js/POIdata.js
+++ js/POIdata.js
@@ -2,27 +2,13 @@ var POILayer;
 
 function getPOIData(){
     var layerURL = "https://developer.cege.ucl.ac.uk:30283/getPOI";
-    var isParking = feature.properties.name.toLowerCase().indexOf("parking"); 
-    console.log(isParking); 
-    switch (true) { 
-        case (isParking > -1): 
-        return L.marker(latlng, 
-    {icon:testMarkerRed}).bindPopup("<b>"+feature.properties.name +"</b>"); 
-        break; 
-
-    default: 
-        return L.marker(latlng, 
-    {icon:testMarkerBlack}).bindPopup("<b>"+feature.properties.name +"</b>"); 
-        break; 
-    };
-
     var testMarkerRed = L.AwesomeMarkers.icon({
     		icon: 'play',
     		markerColor: 'red'
     	});
-    var testMarkerPink = L.AwesomeMarkers.icon({
+    var testMarkerBlack = L.AwesomeMarkers.icon({
     		icon: 'play',
-    		markerColor: 'pink'
+    		markerColor: 'black'
     	});
     $.ajax({url: layerURL, crossDomain: true, success: function(result){
     	console.log(result); // check that the data is correct
@@ -31,11 +17,18 @@ function getPOIData(){
 		{
 		// use point to layer to create the points
 		pointToLayer: function (feature, latlng){
-			// look at the GeoJSON file - specifically at the properties - to see the earthquake magnitude and use a different marker depending on this value
- 			// also include a pop-up that shows the place value of the earthquakes
- 				return L.marker(latlng,  
- 					{icon:isParking}).bindPopup("<b>"+feature.properties.place +"</b>");
- 	
+	       var isParking = feature.properties.name.toLowerCase().indexOf("parking"); 
+            console.log(isParking); 
+            switch (true) { 
+                case (isParking > -1): 
+                return L.marker(latlng, 
+            {icon:testMarkerRed}).bindPopup("<b>"+feature.properties.name +"</b>"); 
+                break; 
+            default: 
+                return L.marker(latlng, 
+            {icon:testMarkerBlack}).bindPopup("<b>"+feature.properties.name +"</b>"); 
+                break; 
+            };
  			}, // end of point to layer
  		}).addTo(mymap);
 		// change the map zoom so that all the data is shown
