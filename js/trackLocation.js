63007b786d410abbde8bd1041d7a4b541cd8a2cb
liuzongshi123 <uceszl9@ucl.ac.uk>
2020-04-01 15:33:27 -0400

    make some changes to code,finally correct the code

--- js/trackLocation.js
+++ js/trackLocation.js
@@ -1,4 +1,3 @@
-
 var trackLocationLayer = []; // create an array to store all the location tracking points
 var geoLocationID; // store the ID of the location tracker so that it can be used to switch the location tracking off
 
@@ -11,25 +10,41 @@ function trackLocation() {
 };
 
 function showPosition(position) {
+	//Show the position on web 
+	document.getElementById('showLocation').innerHTML = "Latitude: " + position.coords.latitude + 
+	"<br>Longitude: " + position.coords.longitude;
+
+	//Create a marker
+	var testMarkerPink = L.AwesomeMarkers.icon({
+    			icon: 'play',
+    			markerColor: 'pink'
+    		});
+
 	// add the new point into the array
 	// the 'push' command just creates another pigeonhole in an array which 
 	// will then contain the new marker point 
 	trackLocationLayer.push(
-		L.marker([position.coords.latitude,position.coords.longitude]).addTo(mymap)
+		L.marker([position.coords.latitude,position.coords.longitude],
+			{icon:testMarkerPink}).addTo(mymap)
 		); 
+	removeLocationLayers();
 };
+
 
 function removeLocationLayers() {
 	// disable the location tracking so that a new point won't be added while you are removing the old points
 	// use the geoLocationID to do this
-	navigator.geolocation.clearWatch(geoLocationID);
+	//navigator.geolocation.clearWatch(geoLocationID);
+	trackLocationLayer.reverse();
 
 	// now loop through the array and remove any points
 	// note that we start with the last point first as if you remove point 1 then point 2 becomes point 1 so 
 	// a loop doesn't work
 	// also we use -1 as arrays in javascript start counting at 0
-	for (var i=trackLocationLayer.length-1; i > -1;i--) {
-		console.log("removing point "+i + " which has coordinates "+trackLocationLayer[i].getLatLng()
-		);
-		mymap.removeLayer(trackLocationLayer[i]);
-};
+
+	for (var i=trackLocationLayer.length-1; i > 0;i--) {
+		console.log("removing point "+i + " which has coordinates "+trackLocationLayer[i].getLatLng());
+	mymap.removeLayer(trackLocationLayer[i]);
+	};	
+	//trackLocation();
+}
