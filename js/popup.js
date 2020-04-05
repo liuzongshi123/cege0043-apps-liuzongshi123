df8d27c226d67ffa48744bafef43c6374a0b9f52
liuzongshi123 <uceszl9@ucl.ac.uk>
2020-04-05 10:57:50 -0400

    add function to determine the cloestest point

--- js/popup.js
+++ js/popup.js
@@ -78,8 +78,14 @@ function checkAnswer(questionID) {
 	//that the user picked
 }
 
+function getDistance() { 
+	alert('getting distance'); 
+	// getDistanceFromPoint is the function called once the distance has been found 
+	navigator.geolocation.getCurrentPosition(closestFormPoint);
+}
+
 //Using the Layer for Distance Measurement – Proximity Alert
-function closestFormPoint() {
+function closestFormPoint(position) {
 	// take the leaflet formdata layer 
 	// go through each point one by one 
 	// and measure the distance to Warren Street 
@@ -89,8 +95,8 @@ function closestFormPoint() {
 
 	// for this example, use the latitude/longitude of warren street 
 	// in your assignment replace this with the user's location
-	var userlat = 51.524048; 
-	var userlng = -0.139924;
+	var userlat = position.coords.latitude; 
+	var userlng = position.coords.longitude;
 	formLayer.eachLayer(function(layer) {
 		var distance = calculateDistance(userlat,userlng,layer.getLatLng().lat, layer.getLatLng().lng, 'K');
 		if (distance < minDistance){
@@ -108,3 +114,22 @@ function closestFormPoint() {
 		}
 	});
 }
+
+// code adapted from https://www.htmlgoodies.com/beyond/javascript/calculate-the-distance-between-two-points-in-your-web-apps.html
+function calculateDistance(lat1, lon1, lat2, lon2, unit) { 
+	var radlat1 = Math.PI * lat1/180; 
+	var radlat2 = Math.PI * lat2/180; 
+	var radlon1 = Math.PI * lon1/180; 
+	var radlon2 = Math.PI * lon2/180; 
+	var theta = lon1-lon2; 
+	var radtheta = Math.PI * theta/180; 
+	var subAngle = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta); 
+	subAngle = Math.acos(subAngle); subAngle = subAngle * 180/Math.PI; // convert the degree value returned by acos back to degrees from radians 
+	dist = (subAngle/360) * 2 * Math.PI * 3956; // ((subtended angle in degrees)/360) * 2 * pi * radius ) 
+												// where radius of the earth is 3956 
+	if (unit=="K") { dist = dist * 1.609344 ;} // convert miles to km 
+	if (unit=="N") { dist = dist * 0.8684 ;} // convert miles to nautical miles 
+	return dist; 
+}
+
+
