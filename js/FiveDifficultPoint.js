24e5465938d6584ff360b456a72f96479c9ab899
liuzongshi123 <uceszl9@ucl.ac.uk>
2020-04-08 12:51:25 -0400

    debug the code

--- js/FiveDifficultPoint.js
+++ js/FiveDifficultPoint.js
@@ -13,18 +13,14 @@ var FiveDifficultPointLayer=[];
 function loadFiveDifficultPoint(result) {
 
 	var feature = result[0].array_to_json[0];
-	alert(feature);
-	console.log(feature);
-		var htmlString = "<DIV id='popup'"+ feature.id + "><h2>" + feature.question_title + "</h2><br>";
-			htmlString = htmlString + "<h3>"+feature.question_text + "</h3><br>";
-			htmlString = htmlString + "<input type='radio' name='answer' id ='"+feature.id+"_1'/>"+feature.answer_1+"<br>";
-			htmlString = htmlString + "<input type='radio' name='answer' id ='"+feature.id+"_2'/>"+feature.answer_2+"<br>";
-			htmlString = htmlString + "<input type='radio' name='answer' id ='"+feature.id+"_3'/>"+feature.answer_3+"<br>";
-			htmlString = htmlString + "<input type='radio' name='answer' id ='"+feature.id+"_4'/>"+feature.answer_4+"<br>";
-			htmlString = htmlString + "</div>";
+		var htmlString = "<div class='form-group d-flex justify-content-center' id='popup'"+ feature.id + "><h4> Question { " + feature.question_title + " }</h4></div>";
+			htmlString = htmlString + "<div class='form-group d-flex justify-content-center'><h5>"+feature.question_text + "</h5></div>";
+			htmlString = htmlString + "<div class='form-group d-flex justify-content-center'><div class='col-sm-6 d-flex justify-content-around'>";
+			htmlString = htmlString + "<label class='radio-inline'><input type='radio' name='answer' id ='"+feature.id+"_1'/>"+feature.answer_1+ "</label>";
+			htmlString = htmlString + "<label class='radio-inline'><input type='radio' name='answer' id ='"+feature.id+"_2'/>"+feature.answer_2+ "</label>";
+			htmlString = htmlString + "<label class='radio-inline'><input type='radio' name='answer' id ='"+feature.id+"_3'/>"+feature.answer_3+ "</label>";
+			htmlString = htmlString + "<label class='radio-inline'><input type='radio' name='answer' id ='"+feature.id+"_4'/>"+feature.answer_4+ "</label>";
+			htmlString = htmlString + "</div></div>";
 			FiveDifficultPointLayer.push(htmlString);
-	document.getElementById('FiveDifficultPoint').innerHTML = htmlString		
-	console.log(FiveDifficultPointLayer);
-	console.log(result);
-	alert(FiveDifficultPointLayer);
+	document.getElementById('FiveDifficultPoint').innerHTML = htmlString;
 }
