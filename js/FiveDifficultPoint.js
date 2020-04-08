ee0e2f83fe6a7918b9a8139a9657df96d6679ab9
liuzongshi123 <uceszl9@ucl.ac.uk>
2020-04-08 13:19:35 -0400

    debug the code

--- js/FiveDifficultPoint.js
+++ js/FiveDifficultPoint.js
@@ -10,28 +10,21 @@ $.ajax({url:"https://developer.cege.ucl.ac.uk:"+ httpsPortNumberAPI +
 
 
 function loadFiveDifficultPoint(result) {
+	var length = result[0].array_to_json.length-1;
+	var QuestionString = "";
+	for (var i=0; i <= length; i++) {
+		var feature = result[0].array_to_json[i];
+			QuestionString = QuestionString +"<div class='form-group d-flex justify-content-center' id='popup'"+ feature.id + "><h4> Question "+ feature.id + " { " + feature.question_title + " }</h4></div>";
+			QuestionString = QuestionString + "<div class='form-group d-flex justify-content-center'><h5>"+feature.question_text + "</h5></div>";
+			QuestionString = QuestionString + "<div class='form-group d-flex justify-content-center'><div class='col-sm-6 d-flex justify-content-around'>";
+			QuestionString = QuestionString + "<label class='radio-inline'><input type='radio' name='answer' id ='"+feature.id+"_1'/>"+feature.answer_1+ "</label>";
+			QuestionString = QuestionString + "<label class='radio-inline'><input type='radio' name='answer' id ='"+feature.id+"_2'/>"+feature.answer_2+ "</label>";
+			QuestionString = QuestionString + "<label class='radio-inline'><input type='radio' name='answer' id ='"+feature.id+"_3'/>"+feature.answer_3+ "</label>";
+			QuestionString = QuestionString + "<label class='radio-inline'><input type='radio' name='answer' id ='"+feature.id+"_4'/>"+feature.answer_4+ "</label>";
+			QuestionString = QuestionString + "</div></div>";
+	};	
 
-	var feature = result[0].array_to_json[0];
-		var htmlString = "<div class='form-group d-flex justify-content-center' id='popup'"+ feature.id + "><h4> Question { " + feature.question_title + " }</h4></div>";
-			htmlString = htmlString + "<div class='form-group d-flex justify-content-center'><h5>"+feature.question_text + "</h5></div>";
-			htmlString = htmlString + "<div class='form-group d-flex justify-content-center'><div class='col-sm-6 d-flex justify-content-around'>";
-			htmlString = htmlString + "<label class='radio-inline'><input type='radio' name='answer' id ='"+feature.id+"_1'/>"+feature.answer_1+ "</label>";
-			htmlString = htmlString + "<label class='radio-inline'><input type='radio' name='answer' id ='"+feature.id+"_2'/>"+feature.answer_2+ "</label>";
-			htmlString = htmlString + "<label class='radio-inline'><input type='radio' name='answer' id ='"+feature.id+"_3'/>"+feature.answer_3+ "</label>";
-			htmlString = htmlString + "<label class='radio-inline'><input type='radio' name='answer' id ='"+feature.id+"_4'/>"+feature.answer_4+ "</label>";
-			htmlString = htmlString + "</div></div>";
-
-	feature = result[0].array_to_json[1];
-			htmlString = htmlString +"<div class='form-group d-flex justify-content-center' id='popup'"+ feature.id + "><h4> Question { " + feature.question_title + " }</h4></div>";
-			htmlString = htmlString + "<div class='form-group d-flex justify-content-center'><h5>"+feature.question_text + "</h5></div>";
-			htmlString = htmlString + "<div class='form-group d-flex justify-content-center'><div class='col-sm-6 d-flex justify-content-around'>";
-			htmlString = htmlString + "<label class='radio-inline'><input type='radio' name='answer' id ='"+feature.id+"_1'/>"+feature.answer_1+ "</label>";
-			htmlString = htmlString + "<label class='radio-inline'><input type='radio' name='answer' id ='"+feature.id+"_2'/>"+feature.answer_2+ "</label>";
-			htmlString = htmlString + "<label class='radio-inline'><input type='radio' name='answer' id ='"+feature.id+"_3'/>"+feature.answer_3+ "</label>";
-			htmlString = htmlString + "<label class='radio-inline'><input type='radio' name='answer' id ='"+feature.id+"_4'/>"+feature.answer_4+ "</label>";
-			htmlString = htmlString + "</div></div>";
-
-	document.getElementById('FiveDifficultPoint').innerHTML = htmlString;
+	document.getElementById('FiveDifficultPoint').innerHTML = QuestionString;
 	document.getElementById("mymap").innerHTML = "#mapContainer { height: 0px; }";
 	document.getElementById('FiveDifficultPoint').style.display = 'block';
 }
